0a458643a9e227e182f77da1c22bda8903c7456c
AE-AE01 <gooding2000@mail.ru>
2026-06-27 16:05:13 +0300

    Update check_position.gd

--- test_json/check_position.gd
+++ test_json/check_position.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@onready var input_LineEdit = $LineEdit
-
 func _ready():
 	print("check_position")
 	var json_description = JSON.parse_string(FileAccess.get_file_as_string("res://test_json/map1.json"))
