50ab118eccf9314969194bd852bd4b91101269db
AE-AE01 <gooding2000@mail.ru>
2026-06-27 16:35:32 +0300

    111

--- test_json/check_position.gd
+++ test_json/check_position.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
+
 func _ready():
-	print("check_position")
-	var json_description = JSON.parse_string(FileAccess.get_file_as_string("res://test_json/map1.json"))
-	print(json_description)
+	for i in range(5):
+		print("check_position")
+		var json_description = JSON.parse_string(FileAccess.get_file_as_string("res://test_json/map1.json"))
+		print(json_description)
 	
-	var a = "12"
-	var access_room = json_description[a]
-	print("Доступные комнаты", access_room)
+		var a = "12"
+		var access_room = json_description[a]
+		print("Доступные комнаты", access_room)
 	
 	
-	# рандомная комната от доступной
-	var random_room = access_room[randi() % access_room.size()]
-	print(random_room)
+		# рандомная комната от доступной
+		var random_room = access_room[randi() % access_room.size()]
+		print(random_room)
